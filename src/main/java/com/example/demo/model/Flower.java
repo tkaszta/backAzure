89d6409844af325106e16114c60a183f5ba24e84
tkaszta <tkaszta@gmail.com>
2023-02-08 19:24:49 +0200

    kommit 2

--- src/main/java/com/example/demo/model/Flower.java
+++ src/main/java/com/example/demo/model/Flower.java
@@ -1,12 +1,12 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Flower {
     private Long id;
     private String name;
