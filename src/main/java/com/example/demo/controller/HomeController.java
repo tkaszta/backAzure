26d279bda48aa9881af59d144e79d749dc1fc2e4
tkaszta <tkaszta@gmail.com>
2023-02-08 22:22:10 +0200

    added repo instance

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.FlowerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    
+
+    private FlowerRepository flowerRepository;
+
+    public HomeController(FlowerRepository flowerRepository) {
+        this.flowerRepository = flowerRepository;
+    }
+
     @GetMapping("/viragok")
     public String home() {
-        return "Hello from Azure App Service szija Bigyo!";
+        return "Hello from Azure App Service szija Bigyoooo!";
     }
 }
